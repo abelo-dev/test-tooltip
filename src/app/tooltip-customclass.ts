a2a9c13c4b30bf6af6e7329ccae2614bff17d78a
abelo-dev <abelodev@hotmail.com>
2024-01-15 06:36:24 +0100

    test

--- src/app/tooltip-customclass.ts
+++ src/app/tooltip-customclass.ts
@@ -7,31 +7,9 @@ import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [NgbTooltipModule],
   templateUrl: './tooltip-customclass.html',
   encapsulation: ViewEncapsulation.None,
+  styleUrls: ["./tooltip-extra-info.scss"],
   styles: `
-		.ngb-tooltip-window{
-			width: 300px;
-		}
-
-		.my-custom-class .tooltip-inner {
-			background-color: grey;
-			width: 300px;
-			max-width: 300px;
-			font-size: 100%;
-
-		}
-
-		.my-custom-class.bs-tooltip-end .tooltip-arrow::before {
-			border-right-color: grey;
-		}
-		.my-custom-class.bs-tooltip-start .tooltip-arrow::before {
-			border-left-color: grey;
-		}
-		.my-custom-class.bs-tooltip-top .tooltip-arrow::before {
-			border-top-color: grey;
-		}
-		.my-custom-class.bs-tooltip-bottom .tooltip-arrow::before {
-			border-bottom-color: grey;
-		}
+		
 	`,
 })
 export class NgbdTooltipCustomclass {}
